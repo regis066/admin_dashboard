bf2e2edd920a30b7f894724347e8eb24b29a60a1
Mugisha Regis <mugisharegis72@gmail.com>
2024-02-05 21:08:43 +0200

    commit

--- server/models/User.js
+++ server/models/User.js
@@ -1,38 +1,39 @@
 import mongoose from "mongoose";
 
 const UserSchema = new mongoose.Schema(
-    {
-        name: {
-            type: String,
-            required: true,
-            min: 2,
-            max: 100
-        },
-        email: {
-            type: String,
-            required: true,
-            max: 50,
-            unique: true,
-            match: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g
-        },
-        password: {
-            type: String,
-            required: true,
-            min: 5
-        },
-        city: String,
-        state: String,
-        country: String,
-        occupation: String,
-        phoneNumber: String,
-        transactions: Array,
-        role: {
-            type: String,
-            enum: ["user", "admin", "superadmin"],
-            default: "admin",
-        },
+  {
+    name: {
+      type: String,
+      required: true,
+      min: 2,
+      max: 100,
     },
-    {timestamps: true}
+    email: {
+      type: String,
+      required: true,
+      max: 50,
+      unique: true,
+      lowercase: true,
+      match: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g,
+    },
+    password: {
+      type: String,
+      required: true,
+      min: 5,
+    },
+    city: String,
+    state: String,
+    country: String,
+    occupation: String,
+    phoneNumber: String,
+    transactions: Array,
+    role: {
+      type: String,
+      enum: ["user", "admin", "superadmin"],
+      default: "admin",
+    },
+  },
+  { timestamps: true }
 );
 
 const User = mongoose.model("User", UserSchema);
