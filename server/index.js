e43c8cb5733ee949a267b235933427c79244693d
Mugisha Regis <mugisharegis72@gmail.com>
2024-02-05 18:37:52 +0200

    admin and user performance pages and overall dashboard

--- server/index.js
+++ server/index.js
@@ -1,23 +1,31 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import mongoose from 'mongoose';
-import cors from 'cors';
-import dotenv from 'dotenv';
-import helmet from 'helmet';
-import morgan from 'morgan';
-import clientRoutes from './routes/client.js';
-import generalRoutes from './routes/general.js';
-import managementRoutes from './routes/management.js';
-import salesRoutes from './routes/sales.js';
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import helmet from "helmet";
+import morgan from "morgan";
+import clientRoutes from "./routes/client.js";
+import generalRoutes from "./routes/general.js";
+import managementRoutes from "./routes/management.js";
+import salesRoutes from "./routes/sales.js";
 
 // models
-import User from './models/User.js';
-import Product from './models/Product.js';
-import ProductStat from './models/ProductStat.js';
-import Transaction from './models/Transaction.js';
-import OverAllStat from './models/OverAllStat.js';
+import User from "./models/User.js";
+import Product from "./models/Product.js";
+import ProductStat from "./models/ProductStat.js";
+import Transaction from "./models/Transaction.js";
+import OverAllStat from "./models/OverAllStat.js";
+import AffiliateStat from "./models/AffiliateStat.js";
 // data
-import { dataUser, dataProduct, dataProductStat, dataTransaction, dataOverallStat } from './data/index.js';
+import {
+  dataUser,
+  dataProduct,
+  dataProductStat,
+  dataTransaction,
+  dataOverallStat,
+  dataAffiliateStat,
+} from "./data/index.js";
 // CONFIGURATION
 dotenv.config();
 const app = express();
@@ -39,7 +47,9 @@ app.use("/sales", salesRoutes);
 // MONGODB
 
 const PORT = process.env.PORT || 9000;
-mongoose.connect(process.env.MONGO_URI).then(() => {
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
     app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
 
     // ONLY ADD DATA ONE TIME
@@ -48,8 +58,6 @@ mongoose.connect(process.env.MONGO_URI).then(() => {
     // ProductStat.insertMany(dataProductStat);
     // Transaction.insertMany(dataTransaction);
     // OverAllStat.insertMany(dataOverallStat);
-
-}
-).catch((error) => console.log(`${error} did not connect`))
-
-
+    // AffiliateStat.insertMany(dataAffiliateStat);
+  })
+  .catch((error) => console.log(`${error} did not connect`));
--- server/routes/general.js
+++ server/routes/general.js
@@ -1,10 +1,15 @@
 import express from "express";
-import { getUser, getAllUsers } from "../controllers/general.js";
+import {
+  getUser,
+  getAllUsers,
+  getDashboardStats,
+} from "../controllers/general.js";
 
 const router = express.Router();
 
 
 router.get("/users", getAllUsers)
 router.get("/user/:id", getUser);
+router.get("/dashboard", getDashboardStats);
 
 export default router;
